f2b67f770a38aac2abc700da34e5f5bc380a3d43
tyagiyuvraj46-maker <tyagiyuvraj46@gmail.com>
2026-08-15 15:52:53 +0530

    Day 06 (2026-08-15): Solve Q11, Q12

---
+++ Day06/q11.c
@@ -0,0 +1,29 @@
+/*
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        if (n % 2 == 0) {
+            printf("%d is even\n", n);
+        } else {
+            printf("%d is odd\n", n);
+        }
+    }
+    return 0;
+}
