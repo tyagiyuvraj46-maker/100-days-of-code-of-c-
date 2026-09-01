72bcf97eb4e6e3e6d80782c866f66abcc4180f02
tyagiyuvraj46-maker <tyagiyuvraj46@gmail.com>
2026-09-01 17:36:34 +0530

    Day 23 (2026-09-01): Solve Q45, Q46

---
+++ Day23/q46.c
@@ -0,0 +1,28 @@
+/*
+Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        printf("*****\n");
+    }
+    return 0;
+}
