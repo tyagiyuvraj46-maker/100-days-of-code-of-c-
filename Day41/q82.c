02ab2d2d1bff1a885ed28b240eba230f3d55d100
tyagiyuvraj46-maker <tyagiyuvraj46@gmail.com>
2026-09-19 15:27:28 +0530

    Day 41 (2026-09-19): Solve Q81, Q82

---
+++ Day41/q82.c
@@ -0,0 +1,23 @@
+/*
+Q82: Print each character of a string on a new line.
+
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    if (fgets(str, sizeof(str), stdin) != NULL) {
+        for (int i = 0; str[i] != '\0' && str[i] != '\n' && str[i] != '\r'; i++) {
+            printf("%c\n", str[i]);
+        }
+    }
+    return 0;
+}
